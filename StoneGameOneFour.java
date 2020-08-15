a42c57e92472a3ac4cc4c854b57c1738d0c07edb
sarweshChitambare <55796768+sarweshChitambare@users.noreply.github.com>
2020-08-15 13:31:42 +0530

    Create StoneGameOneFour.java

---
+++ StoneGameOneFour.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class StoneGameOneFour
+{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int t=0;
+	int count=1;
+	
+	String result=" as";
+	if(n==4) result="alice";
+		else{
+	for(int i=0;i<(n/4)+1;i++)
+	{
+		
+		t=n-4;
+	if(count%2==0)
+	{
+		if(t%2==0) result="Bob";
+	else result="Alice";
+		
+	}
+	else{
+ if(t%2==0) result="Bob";
+ else result="Alice";	
+	}
+	
+	n=t;
+	count++;
+	} 
+  
+		}
+
+
+
+	
+	System.out.println(result);
+}
+}
